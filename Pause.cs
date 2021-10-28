d78273d6fc758c813b6b3f45859caae7622226bd
Seth Grace Banaga <banagas@tcd.ie>
2021-10-29 00:13:17 +0100

    Add files via upload

---
+++ Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onPause()
+    {
+        pause = !pause;
+        if (!pause)
+        {
+            Time.timeScale = 1;
+        }
+        else if (pause)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
